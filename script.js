6d21cdcc74f2ec7ebd0d10e985bdb3303805c47c
anderson-code22 <andersongamasilva5@gmail.com>
2024-12-19 13:29:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function animar() {
+  const btn = document.getElementById("btn-menu");
+  btn.classList.toggle("ativar");
+
+  const menu_ativo = document.getElementById("menu");
+  menu_ativo.classList.toggle("ativar");
+
+  const header_ativo = document.getElementById("header");
+  header_ativo.classList.toggle("ativar");
+
+  
+}
